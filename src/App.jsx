a0b4111d9a33d4d7405f15c6d493ceeb98ff9680
denisesantosdev <denisesantos427@gmail.com>
2023-08-02 21:41:31 -0300

    ✔ feat: make a api call and use state to store the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { React, useEffect, useState } from "react";
+import "./App.css";
 
-import Header from './components/Header'
-import MainArticle from './components/MainArticle'
-import NewArticles from './components/NewArticles'
-import ArticleList from './components/ArticleList'
-
+import Header from "./components/Header";
+import MainArticle from "./components/MainArticle";
+import NewArticles from "./components/NewArticles";
+import ArticleList from "./components/ArticleList";
 
 function App() {
+  const [articlesData, setArticlesData] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json"
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        setArticlesData(
+          data.articles.filter((article,index)=>{
+            return index < 6
+          })
+        )
+      );
+  }, []);
+
+  console.log(articlesData);
 
   return (
     <>
@@ -16,7 +32,7 @@ function App() {
       <NewArticles />
       <ArticleList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,18 @@
 export default function Header() {
-  return (
-    <header className="header">
-      <img
-        src="src/assets/images/logo.svg"
-        alt="Website logo"
-        className="header_logo"
-      />
-      <nav className="header__nav">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">New</a>
-          </li>
-          <li>
-            <a href="#">Popular</a>
-          </li>
-          <li>
-            <a href="#">Trending</a>
-          </li>
-          <li>
-            <a href="#">Categories</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="header">
+            <img src="src/assets/images/logo.svg" alt="Website logo"
+            className="header_logo"
+            />
+            <nav className="header__nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">New</a></li>
+                    <li><a href="#">Popular</a></li>
+                    <li><a href="#">Trending</a></li>
+                    <li><a href="#">Categories</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
