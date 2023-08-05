9619d2f2edda6987c7f5c23b4aeacd8b4152f327
denisesantosdev <denisesantos427@gmail.com>
2023-08-04 21:07:56 -0300

    ✨ feat: add open and close menu buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,42 @@
 export default function Header() {
-    return (
-        <header className="header">
-            <img src="src/assets/images/logo.svg" alt="Website logo"
-            className="header_logo"
-            />
-            <nav className="header__nav">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">New</a></li>
-                    <li><a href="#">Popular</a></li>
-                    <li><a href="#">Trending</a></li>
-                    <li><a href="#">Categories</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <img
+        src="src/assets/images/logo.svg"
+        alt="Website logo"
+        className="header_logo"
+      />
+      <button id="header__open-menu-btn">
+        <img
+          src="./src/assets/images/icon-menu.svg"
+          alt=""
+        />
+      </button>
+      <nav className="header__nav">
+        <button id="header__open-menu-btn">
+          <img
+            src="./src/assets/images/icon-menu-close.svg"
+            alt=""
+          />
+        </button>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">New</a>
+          </li>
+          <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
