f6ad2bd5411bfc2dd15be9e13d3b6c79573768a4
denisesantosdev <denisesantos427@gmail.com>
2023-08-10 20:55:41 -0300

    ✨ feat: article list section styling

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,39 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function ArticleList(props) {
   //console.log(props);
 
   const articleListElement = props.articleList
-  .filter((item,index)=>{
-    return index >= 4
-  })
-  .map((item, index) => {
-    return (
-      <li className="article-list__item" key={index}>
-        <img
-          src={item.urlToImage}
-          alt=""
-          className="article-list__img"
-        />
-        <div className="article-list__text">
-          
+    .filter((item, index) => {
+      return index >= 4;
+    })
+    .map((item, index) => {
+      return (
+        <li
+          className="article-list__item"
+          key={index}>
+          <img
+            src={item.urlToImage}
+            alt=""
+            className="article-list__img"
+          />
+          <div className="article-list__text">
             <Link to={`/article/${item.title}`}>
               <h2 className="article-list__title">{item.title}</h2>
             </Link>
-          
-          <p className="article-list__description">
-           {item.description}
-          </p>
-        </div>
-      </li>
-    );
-  });
+
+           
+          </div>
+        </li>
+      );
+    });
 
   return (
     <section className="article-list">
-      <ul>
-        {articleListElement}
-      </ul>
+      <ul>{articleListElement}</ul>
     </section>
   );
 }
