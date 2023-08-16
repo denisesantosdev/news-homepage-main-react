2d3ef6f54d27300b7ffc9eb86bc973f7bcb5dedf
denisesantosdev <denisesantos427@gmail.com>
2023-08-15 22:55:54 -0300

    ✨ feat: add max-width to wrapper element

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -18,8 +18,7 @@ export default function ArticlePage(props) {
   ); 
 
   return (
-    <>
-    <NewArticles newArticles={props.articlesData}/>
+    <div className="article-page-wrapper">
     <article className="article-page">
       <Link
         className="article-page__back-btn"
@@ -35,6 +34,7 @@ export default function ArticlePage(props) {
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis perspiciatis quod vero officia corporis voluptas quibusdam incidunt libero reprehenderit dolorem, consequuntur eaque reiciendis, natus accusamus minus impedit voluptates facilis repudiandae! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi veritatis animi consectetur sint nihil labore quae corrupti dolorum quia, itaque, laudantium quasi eius distinctio beatae autem magnam expedita dolore. Est ad alias obcaecati quaerat voluptates omnis, odio laborum velit sint delectus repellendus expedita, debitis possimus earum nobis reiciendis cumque! Repellendus accusamus asperiores omnis nesciunt molestiae fugiat quisquam aliquid dolor veniam, ut in harum praesentium nisi exercitationem nobis incidunt perspiciatis sapiente commodi, eum at atque. Ab nemo, enim quod quidem dicta pariatur, repellat quo ad cumque quia porro laboriosam architecto totam. Facilis non suscipit inventore officia repellendus? Impedit dolores expedita magni.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis perspiciatis quod vero officia corporis voluptas quibusdam incidunt libero reprehenderit dolorem, consequuntur eaque reiciendis, natus accusamus minus impedit voluptates facilis repudiandae! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi veritatis animi consectetur sint nihil labore quae corrupti dolorum quia, itaque, laudantium quasi eius distinctio beatae autem magnam expedita dolore. Est ad alias obcaecati quaerat voluptates omnis, odio laborum velit sint delectus repellendus expedita, debitis possimus earum nobis reiciendis cumque! Repellendus accusamus asperiores omnis nesciunt molestiae fugiat quisquam aliquid dolor veniam, ut in harum praesentium nisi exercitationem nobis incidunt perspiciatis sapiente commodi, eum at atque. Ab nemo, enim quod quidem dicta pariatur, repellat quo ad cumque quia porro laboriosam architecto totam. Facilis non suscipit inventore officia repellendus? Impedit dolores expedita magni.</p>
     </article>
-    </>
+    <NewArticles newArticles={props.articlesData}/>
+    </div>
   );
 }
